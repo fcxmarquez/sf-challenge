dd4b8fb4ce29c2f70b614b895f894935c11a084c
fcxmarquez <francisco.marquez.solt@gmail.com>
2025-11-07 16:17:37 -0600

    feat: add Header component to layout

---
+++ components/layout/header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-center p-4 w-full">
+      <h1 className="text-2xl font-bold text-center">Tasks List</h1>
+    </header>
+  );
+};
