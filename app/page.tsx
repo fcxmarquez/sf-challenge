3b1e8618160de444c980fb28c1ec7aac5e0bcca5
fcxmarquez <francisco.marquez.solt@gmail.com>
2025-11-08 15:03:46 -0600

    feat: integrate TaskCreationModal into Home component for task creation

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,29 @@
 
 import { ItemContainer } from '@/components/item-container';
 import { TaskCreator } from '@/components/task-creator';
+import { useState } from 'react';
+import { TaskCreationModal } from '@/components/modals/task-creation';
 
 export default function Home() {
+	const [openCreationDialog, setOpenCreationDialog] = useState(false);
+
+	const handleCreateTask = () => {
+		setOpenCreationDialog(true);
+	};
+
 	return (
-		<div className='flex flex-col items-center justify-center p-8 w-full gap-4 max-w-3xl mx-auto'>
-			<div className='flex justify-between w-full'>
-				<h2 className='font-bold text-center'>
-					Tasks
-				</h2>
+		<>
+			<TaskCreationModal
+				open={openCreationDialog}
+				onOpenChange={setOpenCreationDialog}
+			/>
+			<div className='flex flex-col items-center justify-center p-8 w-full gap-4 max-w-3xl mx-auto'>
+				<div className='flex justify-between w-full'>
+					<h2 className='font-bold text-center'>Tasks</h2>
+				</div>
+				<TaskCreator onCreate={handleCreateTask} />
+				<ItemContainer />
 			</div>
-			<ItemContainer />
-      <TaskCreator onCreate={() => {}} />
-		</div>
+		</>
 	);
 }
