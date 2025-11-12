56b6907df8cf15971e78e4c68fae15c41c1a7870
fcxmarquez <francisco.marquez.solt@gmail.com>
2025-11-11 19:46:12 -0600

    style: update header component to be sticky with backdrop blur effect

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
 	const { setTaskFilter } = useTasksActions();
 
 	return (
-		<header className='p-4 w-full border-b border-border/60 shadow-sm'>
+		<header className='sticky top-0 z-50 p-4 w-full border-b border-border/60 shadow-sm bg-background/80 backdrop-blur-md'>
 			<div className='flex items-center justify-between max-w-2xl mx-auto w-full'>
 				<h1 className='text-2xl font-bold text-center'>Tasks List</h1>
 				<PillGroup>
